fdc04374868bff129b60ffa1b2e0d4ce1e8a6d18
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-22 10:01:07 +0530

    Add project files.

---
+++ DivideArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideArray
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                Console.Write(numbers[i]);  
+            }
+            Console.WriteLine();    
+            Array.Reverse(numbers);
+            Console.WriteLine("Reverse Array=");
+            foreach (int j in numbers)
+            {
+                Console.Write(j);
+
+            }
+            Console.ReadLine();
+
+        }  
+
+    }
+    
+}
